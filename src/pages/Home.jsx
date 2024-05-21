a97320008d8d6134798e2bd97679eba2428a3ed5
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-05-21 13:05:36 +0700

    chage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div>
         <h1>
           <span>Empower Your Business.</span> <br />
-          with a captivating website. Helo Hello
+          with a captivating website.
         </h1>
         <p>Let's unlock the full potential of your brand.</p>
       </div>
