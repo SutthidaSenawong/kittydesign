719e8f952526070a08ea3a2fdb0811730f7db26c
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-05-22 17:13:58 +0700

    delete hello in Home h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
           <span>Empower Your Business.</span> <br />
           with a captivating website.
         </h1>
-        <p>Let's unlock the full potential of your brand. Hello</p>
+        <p>Let's unlock the full potential of your brand.</p>
       </div>
       <Link to="/pricing" className="btn">
         Check the price
